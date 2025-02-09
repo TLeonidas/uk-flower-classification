eee1a441f7704ce7c7ddd1a3a8eec608085f35af
Tales Leonidas <124396089+TLeonidas@users.noreply.github.com>
2025-02-08 19:27:09 -0600

    supressing warnings

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ def load_checkpoint(filepath):
     checkpoint = torch.load(filepath, map_location=torch.device("cpu"))
 
     # Load ResNet50 instead of VGG
-    model = models.resnet50(pretrained=True)
+    model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
 
     # Replace classifier with saved one
     model.fc = checkpoint['classifier']  # ResNet uses `fc`, not `classifier`
